285383c55394c42805134ce8f6f4924663a2e557
User <user@MacBook-Pro.local>
2023-07-26 13:52:14 +0600

    fixed cors error

--- src/main/java/com/liveme/CorsConfig.java
+++ src/main/java/com/liveme/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.liveme.config;
+package com.liveme;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +13,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // Замените на URL вашего локального фронтенда
+        configuration.addAllowedOrigin("http://localhost:3000"); // Замените на URL вашего локального фронтенда
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
