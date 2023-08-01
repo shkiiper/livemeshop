59fd670d3245b14563fb695178f664a569d254b1
User <user@MacBook-Pro.local>
2023-08-01 16:55:30 +0600

    added users

--- src/main/java/com/liveme/config/SecurityConfig.java
+++ src/main/java/com/liveme/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/users/**", "/auth/**", "/roles/**",
                         "/product/**",
-                        "/category/**",
+                        "/categories/**",
                         "/gallery/**",
                         "/thumbnail/**",
                         "/orders/**",
