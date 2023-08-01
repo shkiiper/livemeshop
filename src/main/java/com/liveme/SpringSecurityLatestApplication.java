b0735608e9594b1a24fa3dd9a5e5f8aae5dbfd67
User <user@MacBook-Pro.local>
2023-08-01 15:56:19 +0600

    added users

--- src/main/java/com/liveme/SpringSecurityLatestApplication.java
+++ src/main/java/com/liveme/SpringSecurityLatestApplication.java
@@ -3,6 +3,7 @@ package com.liveme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.liveme.entity.Role;
 import com.liveme.entity.UserInfo;
@@ -17,6 +18,7 @@ public class SpringSecurityLatestApplication {
 
 		RoleRepository roleRepository = context.getBean(RoleRepository.class);
 		UserInfoRepository userInfoRepository = context.getBean(UserInfoRepository.class);
+		PasswordEncoder passwordEncoder = context.getBean(PasswordEncoder.class); // Внедряем PasswordEncoder
 
 		Role adminRole = new Role(1, "Админ");
 		Role managerRole = new Role(2, "Менеджер");
@@ -26,17 +28,19 @@ public class SpringSecurityLatestApplication {
 		roleRepository.save(managerRole);
 		roleRepository.save(clientRole);
 
-		UserInfo admin1 = new UserInfo(1, "shkipper", "shygaev.xxx@gmail.com", "password", "+996507755011",
-				adminRole,
-				null);
-		UserInfo admin2 = new UserInfo(2, "admin", "admin@gmail.com", "password", "+996555666777", adminRole,
-				null);
-		UserInfo manager1 = new UserInfo(3, "manager1", "manager1@gmail.com", "manager1password", "555555555",
-				managerRole, null);
-		UserInfo manager2 = new UserInfo(4, "manager2", "manager2@gmail.com", "manager2password", "666666666",
-				managerRole, null);
-		UserInfo client = new UserInfo(5, "user", "user@gmail.com", "userpassword", "666666666",
-				clientRole, null);
+		// Создаем пользователей и хешируем пароли
+		UserInfo admin1 = new UserInfo(1, "shkipper", "shygaev.xxx@gmail.com", passwordEncoder.encode("password"),
+				"+996507755011", adminRole, null);
+		UserInfo admin2 = new UserInfo(2, "admin", "admin@gmail.com", passwordEncoder.encode("password"),
+				"+996555666777", adminRole, null);
+		UserInfo manager1 = new UserInfo(3, "manager1", "manager1@gmail.com",
+				passwordEncoder.encode("manager1password"),
+				"555555555", managerRole, null);
+		UserInfo manager2 = new UserInfo(4, "manager2", "manager2@gmail.com",
+				passwordEncoder.encode("manager2password"),
+				"666666666", managerRole, null);
+		UserInfo client = new UserInfo(5, "user", "user@gmail.com", passwordEncoder.encode("userpassword"),
+				"666666666", clientRole, null);
 
 		userInfoRepository.save(admin1);
 		userInfoRepository.save(admin2);
